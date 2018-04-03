38f37faf1caa56e47fa8b32254c2e2bdb26e9f5e
Rafał Białek <bialekrafal94@gmail.com>
2018-04-03 22:56:53 +0200

    add if condition

--- module-8/8.6/js/script.js
+++ module-8/8.6/js/script.js
@@ -1,5 +1,12 @@
 var a = prompt("Enter a");
 var b = prompt("Enter b");
 
-var value = (a * a) - (2 * a * b) - (b * b);
+var value = ((a * a) - (2 * a * b) - (b * b));
 
+if (value > 0) {
+	console.log("positive result");
+} else if (value < 0) {
+	console.log("negative result");
+} else {
+	console.log("zero result");
+}
