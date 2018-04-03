2d00f44e97f75d069fcd13feec3416dbac1c8efd
Rafał Białek <bialekrafal94@gmail.com>
2018-04-03 21:56:09 +0200

    end module 8.5

---
+++ module-8/8.5/js/script.js
@@ -0,0 +1,6 @@
+var a = prompt("Enter a in cm");
+var h = prompt("Enter h in cm");
+var triangleArea = (a * h) / 2;
+
+alert("Triangle field with base a: " + a + " cm and height h: " + h + " cm is equal to: " + triangleArea + " cm");
+console.log("Triangle field with base a: " + a + " and height h: " + h + "is equal to: " + triangleArea + " cm");
