23f34aef7c6822fd92d864e3a9a4110a4539813d
Rafał Białek <bialekrafal94@gmail.com>
2018-04-09 13:21:46 +0200

    add two varibles

---
+++ module-9/9.3/scripts.js
@@ -0,0 +1,2 @@
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+var dinosaur = "tricetatops";
