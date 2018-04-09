965340d9dce6c9bd19d3514f31864172ba5816d2
Rafał Białek <bialekrafal94@gmail.com>
2018-04-09 12:12:17 +0200

    add 3 variable & end 9.1

--- module-9/9.1/scripts.js
+++ module-9/9.1/scripts.js
@@ -6,4 +6,10 @@ function getTriangleArea(a,h) {
 	}
 
 }
+var triangle1Area = getTriangleArea(10,15);
+var triangle2Area = getTriangleArea(-10,15);
+var triangle3Area = getTriangleArea(10,-15);
 console.log(getTriangleArea(10, 6));
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
