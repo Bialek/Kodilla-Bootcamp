d670391f9742c23afafbd05575e59bdffa3b112f
Rafał Białek <bialekrafal94@gmail.com>
2018-04-09 12:53:14 +0200

    add if & end 9.2

--- module-9/9.2/scripts.js
+++ module-9/9.2/scripts.js
@@ -1,5 +1,12 @@
 var femaleNames = ["Asia", "Kasia", "Ola", "Jola"];
 var maleNames = ["Piotrek", "Marek", "Arek", "Jarek"];
 var allNames = femaleNames.concat(maleNames);
+var newName = "Marian";
+if (allNames.indexOf(newName) === -1) {
+	allNames.push(newName);
+}	else {
+	console.log("Imie nie może zostać dodane ponownie")
+}
+
 
 console.log(allNames);
