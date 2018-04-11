b539d7bbef281f8d6ea450547e9a021d4bd44632
Rafał Białek <bialekrafal94@gmail.com>
2018-04-11 19:56:45 +0200

    add one loop & end special quest

--- module-9/9.4/scripts.js
+++ module-9/9.4/scripts.js
@@ -1,12 +1,22 @@
 var heigh = prompt("how high Christmas tree you want? ?");
 
 function drawTree(heigh) {
+	var space = "";
+	for (var p = 0; p < heigh; p++) {
+		space +=" "
+		
+	}
+	console.log(space + "*")
+	space = space.substring(1,space.legth);
 	for (var i = 0; i < heigh; i++) {
-		var star = "";
-		for (var j = 0;j <= i; j++) {
-			star += "*";
+		var star = "*";
+		for (var j = 0; j <= i; j++) {
+			
+			star += "**";
 		}
-		console.log(star);
+
+		console.log(space + star);
+		space = space.substring(1,space.legth);
 	}
 }
 
