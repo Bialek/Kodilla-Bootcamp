bdd7005d8b9d996e56890951e7b1e748ba72f799
Rafał Białek <bialekrafal94@gmail.com>
2018-04-25 10:35:12 +0200

    end 11.5

--- module-11/11.5/script.js
+++ module-11/11.5/script.js
@@ -1,8 +1,9 @@
-function button(){
+function button(text){
 	this.text = text || 'hello';
 }
 button.prototype = {
 	create : function() {
+		var self = this;
 		this.$element = $('<button>');
 		this.$element.text(this.text);
 		this.$element.click(function(){
@@ -11,3 +12,7 @@ button.prototype = {
 		$('body').append(this.$element);
 	}
 };
+
+var btn1 = new button('hello!');
+
+btn1.create();
