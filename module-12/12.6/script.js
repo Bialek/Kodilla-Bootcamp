479420876c6b261c3cb068eb5e282e1f48d0e05d
Rafał Białek <bialekrafal94@gmail.com>
2018-05-06 12:46:05 +0200

    12.6 add more info country script add some style

--- module-12/12.6/script.js
+++ module-12/12.6/script.js
@@ -1,5 +1,6 @@
 var url = 'https://restcountries.eu/rest/v2/name/',
-	countriesList = $('#countries');
+	countriesList = $('#countriesList'),
+	$div = $('<div class="countries"></div>');
 
 $('#search').click(searchCountries);
 
@@ -17,15 +18,50 @@ function searchCountries() {
 }
 function showError(){
 	countriesList.empty();
+	$div.empty();
 	$('<li>').text('Not Found').appendTo(countriesList);
+	$('h3').addClass('hidden');
+	$('h3').addClass('hidden');
 }
+
 
 function showCountriesList(resp) {
 	countriesList.empty();
+	$div.empty();
+	$('h2').removeClass('hidden');
+	$('body').append($div);
 
 	resp.forEach(function(item) {
 
+
 		$('<li>').text(item.name).appendTo(countriesList);
 
+		function drawModel(data) {
+			var modelHtml = $('#model').html();
+
+			for (var item in data) {
+				modelHtml = modelHtml.replace('{'+item+'}', data[item]);
+			}
+
+			var $modelHtml = $(modelHtml).removeClass('hidden');
+			$('h3').removeClass('hidden');
+		 		
+			$('.countries').append($modelHtml);
+
+		}
+
+		drawModel({
+		    Country: item.name,
+		    Capital: item.capital,
+		    Area:  item.area,
+		    Population: item.population,
+		    Language: item.languages[0].name,
+		    Currency: item.currencies[0].name,
+		    source: item.flag
+		});
+
 	});
+
 }
+
+
