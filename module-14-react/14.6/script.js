927a189abaa404599aaa5a047cacb2643b419a87
Rafał Białek <bialekrafal94@gmail.com>
2018-05-17 16:33:31 +0200

    test component lifecycle

--- module-14-react/14.6/script.js
+++ module-14-react/14.6/script.js
@@ -1,22 +1,52 @@
 var Counter = React.createClass({
-	getInitialState: function() {
-		return {
-			Counter: 0 
-		};
-	},
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
 
-	increment: function() {
-		this.setState({
-			counter: this.state.counter + 1
-		});
-	}
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
 
-	render: function() {
-		return React.createElement('div', {},
-			React.createElement('span', {}, 'Licznik ' + this.state.counter)
-		);
-	}
+    deccrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },  
+
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter),
+            React.createElement('button', {onClick: this.increment}, '+'),
+            React.createElement('button', {onClick: this.deccrement}, '-')
+        );
+    },
+
+	 componentWillMount: function() {
+    console.log('Before mounting the component');
+    },
+
+    componentDidMount: function() {
+    console.log('After mounting the component');
+    },
+
+    componentWillUpdate: function(nextProps, nextState) {
+    console.log('Current meter status: ' + nextState.counter);
+    },
+
+    componentDidUpdate: function(prevProps, prevState) {
+    console.log('The previous state of the counter: ' + prevState.counter);
+    }
+
 });
 
-var element = React.createElement(Counter);
+var element = React.createElement('div', {},
+    React.createElement(Counter),
+    React.createElement(Counter),
+    React.createElement(Counter)
+);
+
 ReactDOM.render(element, document.getElementById('app'));
