0f26042da8ce7408fbf2b3a7333752f5b7543bf1
Rafał Białek <bialekrafal94@gmail.com>
2018-05-21 20:13:06 +0200

    add promise to 15.5

--- module-15-es6/15.5/public/components/App.js
+++ module-15-es6/15.5/public/components/App.js
@@ -11,32 +11,41 @@ App = React.createClass({
 		this.setState({
 			loading: true
 		});
-		this.getGif(searchingText, function(gif) {
+		this.getGif(searchingText).then(gif => {
 			this.setState({
 				loading: false,
 				gif: gif,
 				searchingText: searchingText
 			});
-		}.bind(this));
+		});
 	},
 
-	getGif: function(searchingText, callback) {
-		var GIPHY_API_URL = 'https://api.giphy.com';
-		var GIPHY_PUB_KEY = 'Zo06Anmw3BLW6loJZvnm97LJHKeUGS2h';
-		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url);
-		xhr.onload = function() {
-			if (xhr.status === 200) {
-				var data = JSON.parse(xhr.responseText).data;
-				var gif = {
-					url: data.fixed_width_downsampled_url,
-					sourceUrl: data.url
+	getGif: function(searchingText) {
+		return new Promise (
+			function(resolve, reject) {
+				var GIPHY_API_URL = 'https://api.giphy.com';
+				var GIPHY_PUB_KEY = 'Zo06Anmw3BLW6loJZvnm97LJHKeUGS2h';
+				var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+				var xhr = new XMLHttpRequest();
+				xhr.open('GET', url);
+				xhr.onload = function() {
+					if (xhr.status === 200) {
+						var data = JSON.parse(xhr.responseText).data;
+						var gif = {
+							url: data.fixed_width_downsampled_url,
+							sourceUrl: data.url
+						};	
+						resolve(gif);
+					} else {
+						reject(new Error(this.statusText));
+					}
 				};
-				callback(gif);
+				xhr.onerror = function() {
+					reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+				};
+				xhr.send();
 			}
-		};
-		xhr.send();
+		);
 	},
 
 	render: function() {
