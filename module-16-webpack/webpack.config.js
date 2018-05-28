4e05b3e991999e11d5385c74728bbbc8b4ac5b3a
Rafał Białek <bialekrafal94@gmail.com>
2018-05-28 09:28:27 +0200

    change config task 16.5

---
+++ module-16-webpack/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = (env) => {
+    return {
+        mode: env || 'production',
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    exclude: ["node_modules"]
+                },
+                {
+                	test: /\.css$/,
+                	use: [
+                		{ loader: 'style-loader'},
+                		{
+                			loader: 'css-loader',
+                			options: {
+                				modules: true
+                			}
+                		}
+                	]
+                }
+            ]
+        }
+    }
+};
