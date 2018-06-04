271193d16c3c26acc45019a01e2670a5b6579d17
Rafał Białek <bialekrafal94@gmail.com>
2018-06-04 18:27:41 +0200

    add getNote and post updateNote end task 17.3

--- module-17-expressjs/17.2/server.js
+++ module-17-expressjs/17.2/server.js
@@ -2,9 +2,40 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    console.log('Otrzymałem żądanie GET do strony głownej');
+    res.send('Hello get');
 });
 
-var server = app.listen(3000, () => {
-    console.log('Przykładowa aplikacja nasłuchuje na http://localhost:3000');
+app.post('/', (req, res) => {
+    console.log('Otrzymałem żądanie POST do strony głównej');
+    res.send('hello POST!');
 });
+
+app.delete('/del_user', (req, res) => {
+    console.log('Otrzymałem żądanie DELETE do strony /del_user');
+    res.send('hello DELETE!');
+});
+
+app.get('/list_user', (req, res) => {
+    console.log('Otrzymałem żądanie GET do strony /list_user');
+    res.send('Strona z listą użytkowników!');
+});
+
+app.get('/ab*cd', (req, res) => {
+    console.log('Otrzymałem żądanie GET do strony /ab*cd');
+    res.send('Wzór pasuje');
+});
+
+// app.get('/:id', (req, res) => {
+//     res.send('Identyfikator, który został dopisany to ' + req.params.id);
+// });
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
+
+app.use(function (req, res, next) {
+    res.status(400).send('400')
+});
+
+app.listen(3000);
--- module-17-expressjs/17.3/server.js
+++ module-17-expressjs/17.3/server.js
@@ -1,10 +1,25 @@
+var fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var stringifyFile;
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+app.use(bodyParser.json());
+
+app.get('/getNote', function(req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        stringifyFile = data;
+        res.send(data);
+    });
 });
 
-var server = app.listen(3000, () => {
-    console.log('Przykładowa aplikacja nasłuchuje na http://localhost:3000');
+app.post('/updateNote/:note', function (req, res) {
+    stringifyFile += req.params.note;
+    fs.writeFile('./test.json', stringifyFile, function(err) {
+        if (err) throw err;
+        console.log('file updated');
+    });
 });
+
+app.listen(3000);
