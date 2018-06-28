b34c9658419f9de9c89546d207b1df426a4252e7
Rafał Białek <bialekrafal94@gmail.com>
2018-06-28 23:45:18 +0200

    add Countries filter

--- module-20-react-router/Database-of-countries synchronized-with-Redux/src/reducers/countries-reducer.js
+++ module-20-react-router/Database-of-countries synchronized-with-Redux/src/reducers/countries-reducer.js
@@ -1,10 +1,10 @@
-import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, DELETE_COUNTRY } from '../actions/actions-countries';
+import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, DELETE_COUNTRY, SET_CONTINENT } from '../actions/actions-countries';
 import countriesData from '../data/countries.json';
 
 const initialState = {
     countries: countriesData,
     selectedCountry: {},
-    visibleCountries: countriesData
+    visibleCountries: []
 };
 
 const countriesReducer = function (state = initialState, action) {
@@ -13,18 +13,22 @@ const countriesReducer = function (state = initialState, action) {
             return Object.assign({}, state, {countries: state.countries});
 
         case GET_COUNTRY:
-            const selectedCountry = state.countries.find(country => country.id === parseInt(action.id));
+            const selectedCountry = state.countries.find(country => country.id == action.id);
             return Object.assign({}, state, {selectedCountry});
-        
+
         case SEARCH_COUNTRIES:
             const foundCountries = state.countries.filter(country => country.name.toLowerCase().includes(action.searchText.toLowerCase()));
             return Object.assign({}, state, {visibleCountries: foundCountries});
-            case DELETE_COUNTRY:
-                const notDeletedCountries = state.countries.filter(country => country.id != action.id);
-                const notDeletedVisibleCountries = state.visibleCountries.filter(country => country.id != action.id);
-                return Object.assign({}, state, {countries: notDeletedCountries, visibleCountries: notDeletedVisibleCountries});
 
-    }   
+        case DELETE_COUNTRY:
+            const notDeletedCountries = state.countries.filter(country => country.id != action.id);
+            const notDeletedVisibleCountries = state.visibleCountries.filter(country => country.id != action.id);
+            return Object.assign({}, state, {countries: notDeletedCountries, visibleCountries: notDeletedVisibleCountries});
+
+        case SET_CONTINENT:
+            const continentCountries = state.countries.filter(country => country.continent === action.name);
+            return Object.assign({}, state, {visibleCountries: continentCountries});
+    }
 
     return state;
 };
--- module-20-react-router/Database-of-countries synchronized-with-Redux/src/routes.js
+++ module-20-react-router/Database-of-countries synchronized-with-Redux/src/routes.js
@@ -5,15 +5,18 @@ import Home from './presentational/home.component';
 import Contact from './presentational/contact.component';
 import NotFound from './presentational/not-found.component';
 import CountryFlagContainer from './containers/flag-container.component';
-import CountryDetailsContainer from './containers/country-detail-container.component'
+import CountryDetailsContainer from './containers/country-detail-container.component';
+import ContinentsContainer from './containers/continents-container.component';
+
 
 export default (
     <Route path='/' component={Navigation}>
-        <IndexRoute  component={Home}/>
-        <Route path='countries'>
+        <IndexRoute component={Home}/>
+        <Route path='countries' >
             <IndexRoute component={CountryFlagContainer}/>
             <Route path='country/:id' component={CountryDetailsContainer}/>
         </Route>
+        <Route path='continents' component={ContinentsContainer}/>
         <Route path='contact' component={Contact}/>
         <Route path='*' component={NotFound}/>
     </Route>
