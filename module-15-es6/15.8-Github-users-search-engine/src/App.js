75d3aaf6327026b6e5a3d52045cb26c1ec65cd02
bialek <bialekrafal94@gmail.com>
2018-08-08 10:34:38 +0200

    add some style to 15.8

--- module-15-es6/15.8-Github-users-search-engine/src/App.js
+++ module-15-es6/15.8-Github-users-search-engine/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './styles.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+
 class App extends React.Component {
   constructor() {
     super();
@@ -16,7 +18,10 @@ class App extends React.Component {
   onSubmit(event) {
     event.preventDefault();
     const {searchText} = this.state;
-    const url = `https://api.github.com/search/users?q=${searchText}`;
+    var url = '';
+    (this.state.searchText === '') 
+    ?  url = 'https://api.github.com/search/users?q=alex'
+    :  url = `https://api.github.com/search/users?q=${searchText}`;
     fetch(url)
       .then(response => response.json())
       .then(responseJson => this.setState({users: responseJson.items}));
@@ -25,16 +30,25 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <form onSubmit={event => this.onSubmit(event)}>
-          <label htmlFor="searchText">Search by user name  </label>
-          <input
-            type="text"
-            id="searchText"
-            onChange={event => this.onChangeHandle(event)}
-            value={this.state.searchText}
-          />
-          <button type="submit">search</button>
-        </form>
+        <nav>
+          <div className="logo">
+            <h1>
+              <i className="fa fa-github"></i>
+              GitHub Users Search Engine
+            </h1>
+          </div>
+          <form onSubmit={event => this.onSubmit(event)}>
+            <label htmlFor="searchText">Search by user name: </label>
+            <input
+              type="text"
+              id="searchText"
+              onChange={event => this.onChangeHandle(event)}
+              placeholder={"e.g. Alex"}
+              value={this.state.searchText}
+            />
+            <button type="submit">search</button>
+          </form>        
+        </nav>
         <UsersList users={this.state.users}/>
       </div>
     );
@@ -48,7 +62,7 @@ class UsersList extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="userContainer">
         {this.users}
       </div>
     );
@@ -56,11 +70,13 @@ class UsersList extends React.Component {
 }
 
 class User extends React.Component {
-  render() {
+  render() { 
     return (
-      <div className="user">
-        <img src={this.props.user.avatar_url} alt="avatar"/>
-        <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+      <div className="user">   
+        <a href={this.props.user.html_url} target="_blank">
+          <img src={this.props.user.avatar_url} alt="avatar"/>
+          {this.props.user.login}
+        </a>
       </div>
     );
   }
