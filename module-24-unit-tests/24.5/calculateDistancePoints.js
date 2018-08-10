52fdf5203fa63619c913b8218b33084366843873
bialek <bialekrafal94@gmail.com>
2018-08-10 17:00:08 +0200

    24.5 add logic to calculators

--- module-24-unit-tests/24.5/calculateDistancePoints.js
+++ module-24-unit-tests/24.5/calculateDistancePoints.js
@@ -1,17 +1,17 @@
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
     switch (hillSize) {
         //normal hillSize
-        case 1:
-            return
+        case 0:
+            return (60 + ((distance - kPoint) * 2));
         //large hillSize
-        case 2:
-            return
+        case 1:
+            return (60 + ((distance - kPoint) * 1.8));
         //ski flying hillSize
-        case 3:
-            return    
+        case 2:
+            return (120 + ((distance - kPoint) * 1.2));
         default:
             return "wrong hillSize";
     }
   };
-  
+
   module.exports = calculateDistancePoints;
--- module-24-unit-tests/24.5/calculateStylePoints.js
+++ module-24-unit-tests/24.5/calculateStylePoints.js
@@ -1,5 +1,14 @@
 const calculateStylePoints = (styleNotes) => {
-    ...
+    const remove = (array, item) => {
+        const index = array.indexOf(item);
+        if (index !== -1) array.splice(index, 1);
+        return array;
+    }
+
+    remove(styleNotes, Math.max(...styleNotes));
+    remove(styleNotes, Math.min(...styleNotes));
+
+    return styleNotes;
   };
-  
+
   module.exports = calculateStylePoints;
